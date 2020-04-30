33107924f9473a6e5eeb4e47d1127fb6a6eb749c
BaskaKlim <baska.klimekova@gmail.com>
2020-04-30 14:23:09 +0200

    beforeAll config, given.pathParam parameter

--- RestAPI_SpellTests_day1/src/test/java/tests/SpellTests.java
+++ RestAPI_SpellTests_day1/src/test/java/tests/SpellTests.java
@@ -1,11 +1,20 @@
 package tests;
 
 import org.junit.jupiter.api.*;
+import io.restassured.*;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 
 public class SpellTests {
+
+    @BeforeAll
+    static void config(){
+        baseURI = "http://localhost";
+        port= 3000;
+    }
+
 
     @Test
     /** SMOKE REST ASURED TEST - testujem endpoint Spells ci odpovie na request - code200 a ci obsahuje konkretny object spell **/
@@ -13,8 +22,8 @@ public class SpellTests {
         //arrange - given     dana url
         //act   - when    zavolam GET REQUEST
         //assert   - then     statuscode bude 200
-        given().baseUri("http://localhost:3000/spells/5b74ef813228320021ab624c")
-                .when().get()
+        given().pathParam("spellId","5b74ef813228320021ab624c")
+                .when().get("/spells/{spellId}")
                 .then().statusCode(200)
                 .body("spell",equalTo("Crucio"))
                 .body("type",equalTo("Curse"))
