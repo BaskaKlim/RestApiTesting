a55f00463c779e133d4820be2a5fe089a154acb1
BaskaKlim <baska.klimekova@gmail.com>
2020-04-30 13:21:32 +0200

    Rest API sortingHat website - test structure template

---
+++ RestAPI_SortingHatTest_day1/src/test/java/tests/SortingHatTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.collection.IsIn.*;
+import static org.hamcrest.collection.IsMapContaining.*;
+import static org.hamcrest.text.IsEmptyString.*;
+
+public class SortingHatTest {
+
+}
