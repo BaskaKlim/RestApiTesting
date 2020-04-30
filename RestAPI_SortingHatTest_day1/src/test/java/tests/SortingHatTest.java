31638d31d08492ecb98e5280a1ff2b44a59a872e
BaskaKlim <baska.klimekova@gmail.com>
2020-04-30 13:52:22 +0200

    test for validate request body values via hasKey function hamcrest lib

--- RestAPI_SortingHatTest_day1/src/test/java/tests/SortingHatTest.java
+++ RestAPI_SortingHatTest_day1/src/test/java/tests/SortingHatTest.java
@@ -11,9 +11,8 @@ import static org.hamcrest.text.IsEmptyString.*;
 
 public class SortingHatTest {
 
-
     @Test
-    /** REST ASURED TEST - testujem endpoint SortingHat ci odpovie na request - code200**/
+    /** SMOKE REST ASURED TEST - testujem endpoint SortingHat ci odpovie na request - code200**/
     void itShouldReturnStatusCode200() {
         //arrange - given     dana url
         //act   - when    zavolam GET REQUEST
@@ -22,7 +21,17 @@ public class SortingHatTest {
                 .when().get()
                 .then().statusCode(200);
 
+    }
 
+    @Test
+    /** validate body request value - testujem odpoved, co mi pride v requeste**/
+
+    void itShouldReturnCorrectKeys() {
+        given().baseUri("http://localhost:3000/sortingHat")
+                .when().get()
+                .then().log().body()      //vypisanie requestu do koncoly
+                .body("", hasKey("sortingHatSays"))
+                .body("", hasKey("house"));
     }
 
 }
