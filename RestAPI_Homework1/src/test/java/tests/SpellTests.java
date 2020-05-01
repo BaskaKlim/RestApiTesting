3a48df81f58e82fc98c5be6689232a10febb1661
BaskaKlim <baska.klimekova@gmail.com>
2020-05-01 23:33:22 +0200

    vytiahnem si vsektky hodnoty z Response na GET request obsahujuce konkretny atriput skry jsonPat

--- RestAPI_Homework1/src/test/java/tests/SpellTests.java
+++ RestAPI_Homework1/src/test/java/tests/SpellTests.java
@@ -1,9 +1,13 @@
 
 package tests;
 
+import java.util.*;
+import org.hamcrest.collection.*;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 public class SpellTests {
     //TODO: dotiahnite si všetky spells a overte že zoznam všetkých kúziel obsahuje Avada Kedavra, Crucio a Imperio
@@ -17,8 +21,16 @@ public class SpellTests {
     }
 
     @Test
-    void itShoudlContainAvadaKedavra() {
+    void itShoudlContainsAvadaKedavraSpellinListOfSpells() {
+        //zadefinujem si, co hladam
+        String expectedSpell = "Avada Kedavra";
 
+        //vytiahnem  si vsetky hodnoty z responde na GET request, konkretne atribut spell kazdej odpovede
+        List<String> allSpellsOnWeb =  when().get()
+                .then().extract().response()
+                .jsonPath().getList("$.spell");
+
+       
     }
 }
 
