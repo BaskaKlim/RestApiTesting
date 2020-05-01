99642b9e299e9142e0b79ab9461b9b1cbb84cadb
BaskaKlim <baska.klimekova@gmail.com>
2020-05-01 23:04:04 +0200

    configuration URL, port and basePath

--- RestAPI_Homework1/src/test/java/tests/SpellTests.java
+++ RestAPI_Homework1/src/test/java/tests/SpellTests.java
@@ -1,7 +1,24 @@
 
 package tests;
 
+import org.junit.jupiter.api.*;
 
-public class SpellTests{
-  //TODO: dotiahnite si všetky spells a overte že zoznam všetkých kúziel obsahuje Avada Kedavra, Crucio a Imperio
+import static io.restassured.RestAssured.*;
+
+public class SpellTests {
+    //TODO: dotiahnite si všetky spells a overte že zoznam všetkých kúziel obsahuje Avada Kedavra, Crucio a Imperio
+
+    @BeforeAll
+    static void config() {
+        baseURI = "http://localhost";
+        port = 3000;
+        basePath = "/spells";
+
+    }
+
+    @Test
+    void itShoudlContainAvadaKedavra() {
+
+    }
 }
+
