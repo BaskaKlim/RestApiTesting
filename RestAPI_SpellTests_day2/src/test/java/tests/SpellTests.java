60ef5344f16f9190fb203220f868cc4a7b4cc8cc
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 14:11:23 +0200

    template for spell tests

---
+++ RestAPI_SpellTests_day2/src/test/java/tests/SpellTests.java
@@ -0,0 +1,27 @@
+
+package tests;
+
+import java.util.*;
+import org.hamcrest.collection.*;
+import org.junit.jupiter.api.*;
+
+import exceptions.*;
+
+import static io.restassured.RestAssured.*;
+import static java.util.stream.Collectors.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SpellTests {
+
+    @BeforeAll
+    static void config() {
+        baseURI = "http://localhost";
+        port = 3000;
+        basePath = "/spells";
+
+
+    }
+
+}
+
