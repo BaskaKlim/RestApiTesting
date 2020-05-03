fb65f37448904745b1362b79ecaf721474cae5c2
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 14:00:53 +0200

    using custom exception message and placeholder %s for string

--- RestAPI_Homework1/src/test/java/exceptions/SpellNotFundException.java
+++ RestAPI_Homework1/src/test/java/exceptions/SpellNotFundException.java
@@ -4,6 +4,7 @@ public class SpellNotFundException extends Exception {
 
     public SpellNotFundException(String message) {
         //volam konstruktor materskej triedy Exception
-        super(message);
+        //pouzijem placeholder %s a prihodim message
+        super(String.format("Spell '%s' not found in list of spells", message));
     }
 }
