8544181f4252bbaade976a62302faad077b0fe23
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 16:08:41 +0200

    using JAVA FAKER as dependency, using faker data

--- RestAPI_SpellTests_day2/src/test/java/tests/SpellTests.java
+++ RestAPI_SpellTests_day2/src/test/java/tests/SpellTests.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.hamcrest.collection.*;
 import org.junit.jupiter.api.*;
 
+import com.github.javafaker.*;
 import exceptions.*;
 import models.*;
 
@@ -23,12 +24,24 @@ public class SpellTests {
 
     }
 
+   /* @BeforeEach
+    static void resetSpells() {
+        // zavolam konkretny endpoint tak si vymazem a zresetujem kuzla: https://localhost:3009/spells/actions/reset
+        when().get("/actions/deleteAll")
+                .then().statusCode(200);
+
+        when().get("/actions/reset")
+                .then().statusCode(200);
+
+    }
+    */
+
     @Test
     void itShoulFilterSpellsBasedOnQueryType() {
         //1. nastavim parametre cez query a vytiahnem si list kuzel, ktory ich splna
         List<Spell> spells = given().queryParam("type", "Curse")
                 .when().get()
-                .then().extract().jsonPath().getList("$",Spell.class);
+                .then().extract().jsonPath().getList("$", Spell.class);
 
         // 2. overit ze kazde kuzlo je typu Curse
         spells.forEach(spell -> assertThat(spell.getType(), equalTo("Curse")));
@@ -37,21 +50,23 @@ public class SpellTests {
         assertThat(spells, hasSize(greaterThan(0)));
     }
 
-
     // TODO: pridat nove kuzlo
     @Test
-    void itShouldAddNewSpell(){
+    void itShouldAddNewSpell() {
+
+        Faker faker = new Faker();
         //1. vytvorit objektu noveho kuzla
-        HashMap<Object,Object> newSpell = new HashMap<>();
-        newSpell.put("spell","Corona");
-        newSpell.put("effect","sneezing forever");
-        newSpell.put("type","Curse");
-        newSpell.put("isUnforgivable","true");
+        HashMap<Object, Object> newSpell = new HashMap<>();
+        newSpell.put("spell", faker.harryPotter().character());
+        newSpell.put("effect", faker.harryPotter().quote());
+        newSpell.put("type", "Curse");
+        newSpell.put("isUnforgivable", "true");
 
         //2. pomocou POST poslem toto kuzlo serveru
-          // treba za given nastavit hlavicku
-        given().header("Content-Type","application/json")
-                .body(newSpell).when().post().then().statusCode(201)
+        // treba za given nastavit hlavicku
+        given().header("Content-Type", "application/json")
+                .body(newSpell).log().body() //zalogujem si co posielam na server
+                .when().post().then().statusCode(201)
                 .body("message", equalTo("Spell created"))
                 .body("spell.id", not(emptyOrNullString()));
     }
