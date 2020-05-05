0599bce8222cbaf8625d46cf778c1d0bd94af1a1
BaskaKlim <baska.klimekova@gmail.com>
2020-05-05 15:30:36 +0200

    get Token from login page via auth preemptive basic and then use token and get List of Houses

--- RestAPI_Homework2/src/test/java/tests/HousesTests.java
+++ RestAPI_Homework2/src/test/java/tests/HousesTests.java
@@ -12,18 +12,35 @@ public class HousesTests {
     static void config() {
         baseURI = "http://localhost";
         port = 3000;
-        basePath = "/houses";
+
     }
 
     //TODO: Skúste navhrnúť testy na endpoint Houses, postupujte podľa dokumentácie, nájdete ju tu https://documenter.getpostman.com/view/6199862/SzYewFs9?version=latest
     @Test
     void itShouldReturnStatusCode403AfterGETRequestWithoutAuthorisation() {
         //1. overim si, ze ci ked dam get request prejde mi poziadavka
-        when().get()
+        when().get("/houses")
                 .then().statusCode(403)
                 .and().body("message", equalTo("Sorry Wizard you dont have TOKEN"));
         //2. zistim, ze nie, a ze hlasi error 403 - forbidden pristup, potrebujem sa autorizovat
 
     }
-    
+    @Test
+    void itShouldReturnListOfAllHousesInHarryPotter() {
+        //1. ziskam token
+        String token = given().auth().preemptive().basic("admin", "supersecret")
+                .when().get("/login")
+                .then().extract().jsonPath().get("token");
+
+        //2. vytiahnem si list houses
+
+      List<String> allHousesByName=  given()
+               .header("Authorization", "Bearer ".concat(token))
+                .when().get("/houses")
+                .then().extract().jsonPath().getList("name");
+
+      System.out.println(allHousesByName);
+
+
+    }
 }
