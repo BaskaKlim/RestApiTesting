e9f491d8885830ec0ec389be099c3651d7b4ab16
BaskaKlim <baska.klimekova@gmail.com>
2020-05-05 14:27:35 +0200

    resetSpells() and assert the size of List of Spells

---
+++ RestAPI_Homework2/src/test/java/tests/SpellTests.java
@@ -0,0 +1,50 @@
+
+package tests;
+
+import java.util.*;
+import org.hamcrest.collection.*;
+import org.junit.jupiter.api.*;
+
+import com.github.javafaker.*;
+import exceptions.*;
+import io.restassured.http.*;
+import models.*;
+
+import static io.restassured.RestAssured.*;
+import static java.util.stream.Collectors.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SpellTests {
+
+    @BeforeAll
+    static void config() {
+        baseURI = "http://localhost";
+        port = 3000;
+        basePath = "/spells";
+
+    }
+
+    @BeforeEach
+    void resetSpells() {
+        // zavolam konkretny endpoint tak si zresetujem kuzla: https://localhost:3009/spells/actions/reset
+        when().get("/actions/reset")
+                .then().statusCode(200);
+
+    }
+
+    @Test
+        // TODO: overte, že po zavolaní endpointu /spells/actions/reset a následnom zavolaní spells  sa vráti presne 151 kúziel
+    void itShouldResetSpells() {
+        when().get("actions/reset")
+                .then().statusCode(200);
+
+        List<Spell> resetSpells = when().get()
+                .then().extract().jsonPath().getList("");
+        assertThat(resetSpells, hasSize(151));
+
+    }
+
+
+}
+
